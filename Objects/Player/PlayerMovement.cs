96ed846fd48b5cd862bdd24ba8e1e0fa5ed6662f
CookieRat289 <152470660+CookieRat289@users.noreply.github.com>
2024-12-02 15:04:58 +1100

    v0.1a - CharacterBody2D
    Refined player movement for CharacterBody2D nodes.

--- Objects/Player/PlayerMovement.cs
+++ Objects/Player/PlayerMovement.cs
@@ -1,29 +1,25 @@
 using Godot;
 using System;
 
-public partial class PlayerMovement : Node2D
+public partial class PlayerMovement : CharacterBody2D
 {
 	[Export]
-	public int playerSpeed = 3;
+	public int playerSpeed = 100;
+	
+	public int spdMagnifier = 1;
 
-	public Vector2 playerVel;
-
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	public void GetInput()
 	{
-	}
+		Vector2 inputDir=Input.GetVector("playerLeft","playerRight","playerUp","playerDown");
+		
+		if (Input.IsActionPressed("playerSprint")) spdMagnifier = 2;
+		else spdMagnifier = 1;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+        Velocity = inputDir * playerSpeed * spdMagnifier;
+	}
+	public override void _PhysicsProcess(double delta)
 	{
-		MovePlayer(); // Move the player
-	}
-
-    private void MovePlayer() {
-		Vector2 inputDir = Input.GetVector("PlayerLeft", "PlayerRight", "PlayerUp", "PlayerDown"); // Get the normalised direction of input
-		if (!inputDir.IsZeroApprox()) { // If there is a direction (i.e. the player is supposed to move)...
-			playerVel = inputDir * playerSpeed; // Set the velocity of the player to the direction with the provided speed
-			Position += playerVel; // Add the velocity to the position of the player container
-		}
+		GetInput();
+		MoveAndSlide();
 	}
 }
