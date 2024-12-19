83c56bb87d4635be66630f9969064926b4efa250
CookieRat289 <152470660+CookieRat289@users.noreply.github.com>
2024-12-19 14:59:00 +1100

    v0.1 - Button Toggle Menus
    The primary menu and its tabs can now be opened by clicking on the buttons or by pressing the associated keys.

---
+++ Objects/UI/HUD/PrimaryMenuButton.cs
@@ -0,0 +1,52 @@
+using Godot;
+using System;
+
+public partial class PrimaryMenuButton : Button
+{
+	[Export] private Node2D _primaryContainer;
+	[Export] private TabContainer _tabContainer;
+	[Export] private String _inputName;
+	[Export] private int _tabIndex;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		Pressed += OnClick;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if (_inputName != "closeMenu") { // If this is not the close button...
+			if (Input.IsActionJustPressed(_inputName)) {
+				ToggleMenus();
+			}
+		}
+	}
+
+	private void OnClick() {
+		if (_primaryContainer.Visible) { // If the primary container is already open...
+			if (_inputName == "closeMenu") { // If the close button was clicked...
+				_primaryContainer.Visible = false; // Close the menu
+			} 
+			return; // Ignore the button press
+		} else { // Otherwise...
+			ToggleMenus();
+		}
+	}
+
+	private void ToggleMenus() {
+		if (_primaryContainer.Visible) { // If the primary container is already open...
+			if (_inputName == "uiPause") { // If the pause button was pressed when the menu was open...
+				_primaryContainer.Visible = false; // Close the menu
+				return;
+			}
+
+			if (_tabContainer.CurrentTab == _tabIndex) _primaryContainer.Visible = false; // If the tab opened was the one already opened, close the menu
+			else _tabContainer.CurrentTab = _tabIndex; // Otherwise, set the tab
+		} else { // Otherwise...
+			_primaryContainer.Visible = true;
+			_tabContainer.CurrentTab = _tabIndex;
+		}
+	}
+}
