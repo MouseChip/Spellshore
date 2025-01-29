b570c40b252c8819403985a902feb0e47139626f
CookieRat289 <152470660+CookieRat289@users.noreply.github.com>
2025-01-29 14:24:15 +1100

    v0.1b - Basic Ground Tiles
    The basic ground tiles have been made for simplistic map design (no corners, edges, transitions, etc.). This includes three grass variants, three dirt variants, two sand variants, and a water tile.

--- Objects/Player/VisualiseCast.cs
+++ Objects/Player/VisualiseCast.cs
@@ -6,12 +6,10 @@ public partial class VisualiseCast : ProgressBar
 	// Called when the node enters the scene tree for the first time.
 	private PlayerData _playerData = GD.Load<PlayerData>("res://Objects/Player/playerData.tres"); // Load the player data
 	private FishingRod _fishingRod;
-	private StyleBoxFlat _progressStyle;
 
 	public override void _Ready()
 	{
 		_fishingRod = GetParent().GetParent().GetNode<FishingRod>("FishingRod"); // Get the player's fishing rod object
-		_progressStyle = GetThemeStylebox("fill") as StyleBoxFlat; // Get the style box for the fill of the theme
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,7 +19,6 @@ public partial class VisualiseCast : ProgressBar
 			Visible = true; // Display the progress bar
 
 			Value = _fishingRod.CastLevel; // Set the value of the bar to the cast level
-			_progressStyle.BgColor = Colors.Orange.Lerp(Colors.Green, (float)(Value / MaxValue)); // Make the colour of the bar dependent on the value
 		} else {
 			Visible = false;
 		}
